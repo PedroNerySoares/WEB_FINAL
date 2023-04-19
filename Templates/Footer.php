6bd94dc9c4c101c1deab71da5074f4e5ecd64946
Pedro <pedrohnery@gmail.com>
2023-04-18 23:54:42 -0300

    Adicionado PHP e imgs

---
+++ Templates/Footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <span>Desenvolvido por: </span>
+    <span>Pedro Nery </span>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include_once("./Templates/Header.php");
+?>
+
+<h1>testeando</h1>
+
+<?php
+    include_once("./Templates/Footer.php");
+?>
