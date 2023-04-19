af1fc9cd710234525ab026667a1b560e6f7be6a7
Pedro <pedrohnery@gmail.com>
2023-04-19 02:22:38 -0300

    Cards

--- index.php
+++ index.php
@@ -1,43 +1,27 @@
 <?php
-include_once("./Templates/Header.php");
 
-$qtde = count($books);
-echo ("quantidade de livro:$qtde");
+include_once("./Templates/Header.php");
 
 ?>
 
-<?php foreach ($books as $book) : ?>
-    <div class="card">
-        <img class="card-img-top" src="<?=$book["IMAGEM"]?> alt="Card image cap">
+<section>
+    <?php foreach ($books as $book) : ?>
+        <div class="card">
+            <img class="card-img-top" src="<?= $book["IMAGEM"] ?> alt=" Card image cap">
 
-        <div class="card-body">
-            <h5 class="card-title"><?=$book["Nome"]   ?></h5>
-            <p class="card-text">Livro em otimo estado, guardao fora de umidade e sol, lindo somente uma vez </p>
-            <p>Doador: Pedro Nery</p>
-            <a href="#" class="btn btn-danger">Eu quero!</a>
+            <div class="card-body">
+                <h5 class="card-title"><?= $book["Nome"]   ?></h5>
+                <p class="card-text"><?= $book["DESCRICAO"]  ?></p>
+                <p>Doador:<?= $book["DOADOR"]  ?></p>
+                <a href="#" class="btn btn-danger">Eu quero!</a>
+            </div>
+
         </div>
 
-    </div>
 
 
-
-<?php endforeach; ?>
-
-<!-- 
-
-
-
-<div class="card">
-    <img class="card-img-top" src="<?= $BASE_URL ?>img/LBDV.jpg" alt="Card image cap">
-                                    
-    <div class="card-body">
-        <h5 class="card-title">Lado Bom da Vida</h5>
-        <p class="card-text">Qualidade mediana </p>
-        <p>Doador: Dath Vader</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-    
-</div> -->
+    <?php endforeach; ?>
+</section>
 
 <?php
 include_once("./Templates/Footer.php");
