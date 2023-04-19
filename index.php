bd1c724aa382194b052b5c90c361e110b2a4ee35
Pedro <pedrohnery@gmail.com>
2023-04-19 00:42:42 -0300

    Adicionado mais um exemplo

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
-    include_once("./Templates/Header.php");
+include_once("./Templates/Header.php");
+
 ?>
 
-<h1>testeando</h1>
+<div class="card" style="width: 20rem; height: auto;">
+    <img class="card-img-top" src="<?= $BASE_URL ?>img/hp_2.jpg" alt="Card image cap">
+                                    
+    <div class="card-body">
+        <h5 class="card-title">Harry Potter - Camera Secreta</h5>
+        <p class="card-text">Livro em otimo estado, guardao fora de umidade e sol, lindo somente uma vez </p>
+        <p>Doador: Pedro Nery</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    
+</div>
+
+
+<div class="card" style="width: 20rem; height: auto;">
+    <img class="card-img-top" src="<?= $BASE_URL ?>img/LBDV.jpg" alt="Card image cap">
+                                    
+    <div class="card-body">
+        <h5 class="card-title">Lado Bom da Vida</h5>
+        <p class="card-text">Qualidade mediana </p>
+        <p>Doador: Dath Vader</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    
+</div>
 
 <?php
-    include_once("./Templates/Footer.php");
+include_once("./Templates/Footer.php");
 ?>
