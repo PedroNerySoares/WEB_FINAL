efdba81a054469cd58fd8f265859377adf3f8130
Pedro <pedrohnery@gmail.com>
2023-04-23 12:59:03 -0300

    LivroDefault e wahts

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
 include_once("./Templates/Header.php");
-include_once("./Config/BuscarLivros.php")
+include_once("./Config/BuscarLivros.php");
+// include_once("./Config/emai.php")
+
+$api_what = "https://api.whatsapp.com/send?phone=5521970351912&text=";
+$string = "Olá, ví seu anúncio do Livro (XXXXXXXXXXX) no AdoBook. Ainda esta disponível? ";
+echo ($api_what . str_replace(" ", "%20", $string));
+
 ?>
 <div class="container_pagina">
 
@@ -10,7 +16,11 @@ include_once("./Config/BuscarLivros.php")
         <form id="filtro-form" method="GET">
             <div class="mb-3">
 
-                <?php include_once("Templates/select.html") ?>
+                <?php include_once("Templates/selectCategoria.html") ?>
+            </div>
+            <div class="mb-3">
+
+                <?php include_once("Templates/selectCondicao.html") ?>
             </div>
 
             <button type="submit" class="mb-3 btn btn-primary">Aplicar filtro</button>
@@ -24,7 +34,12 @@ include_once("./Config/BuscarLivros.php")
         <?php foreach ($books as $book) : ?>
 
             <div class="card">
-                <img class="card-img-top" src="<?= $book["IMAGEM"] ?> alt=" Card image cap">
+                <!-- <img class="card-img-top" src="<?= $book["IMAGEM"] ?> alt=" Card image cap"> -->
+                <img class="card-img-top" "> 
+                
+                <img class=" card-img-top" src="<?= $book["IMAGEM"] ?> 
+                     alt=" Card image cap" onerror="this.onerror=null;this.src='./img/LivroDefault.jpg';">
+
                 <div class="card-body">
                     <h5 class="card-title"><?= $book["Nome"]   ?></h5>
                     <p class="card-text"><?= $book["DESCRICAO"]  ?></p>
