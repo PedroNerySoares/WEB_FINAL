27c334cfd3dbf1206efe773a87c054cd976c4781
pedro7vilela <pedro,vilela7@gmail.com>
2023-04-22 23:15:20 -0300

    nada d+

--- CadastrarLivros.php
+++ CadastrarLivros.php
@@ -15,46 +15,51 @@ include_once("./Templates/Header.php")
         <img id="card-img" src="placeholder.jpg" style="display: none;">
     </div>
     <form id="create-form" action="<?= $BASE_URL ?>config/criarLivro.php" method="POST">
-        <input type="hidden" name="type" value="create">
-        <div class="form-group col-md-4">
-            <label for="name">Nome do Livro:</label>
-            <input type="text" class="form-control" id="nomeLivro" name="nomeLivro" placeholder="Digite o nome do livro" required>
-        </div>
-
-        <div class="form-group col-md-3">
-            <?php include_once("Templates/select.html") ?>
-        </div>
-
-
-        <div class="form-group col-md-4">
-            <label for="phone">URL da Foto:</label>
-            <input type="text" class="form-control" id="urlImage" name="urlImage" onchange="atualizarImagem()">
-
-        </div>
-
-        <div class="form-group col-md-4">
-            <label for="observations">Observações:</label>
-            <textarea type="text" class="form-control" id="observations" name="observacao" placeholder="Insira as observações" rows="3"></textarea>
-        </div>
-        <div class=" col-md-6 row">
-            <div class="form-group col-md-3">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nomeDoador" name="nomeDoador" placeholder="Digite o primeiro nome" required>
-            </div>
-            <div class="form-group col-md-3">
-                <label for="nome">Sobrenome:</label>
-                <input type="text" class="form-control" id="sobreNomeDoador" name="sobreNomeDoador" placeholder="Digite o Sobrenome" required>
+        <fieldset>
+            <input type="hidden" name="type" value="create">
+            <div class="form-group col-md-4">
+                <label for="name">Nome do Livro:</label>
+                <input type="text" class="form-control" id="nomeLivro" name="nomeLivro" placeholder="Digite o nome do livro" required>
             </div>
 
-        </div>
-        <div class="form-group col-md-4">
-            <label for="phone">Telefone do contato:</label>
-            <input type="text" class="form-control" id="celular" name="celular" placeholder="Digite o telefone" required>
-        </div>
+            <div class="form-group col-md-3">
+                <?php include_once("Templates/select.html") ?>
+            </div>
 
 
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
+            <div class="form-group col-md-4">
+                <label for="phone">URL da Foto:</label>
+                <input type="text" class="form-control" id="urlImage" name="urlImage" onchange="atualizarImagem()">
 
+            </div>
+
+            <div class="form-group col-md-4">
+                <label for="observations">Observações:</label>
+                <textarea type="text" class="form-control" id="observations" name="observacao" placeholder="Insira as observações" rows="3"></textarea>
+            </div>
+            <div class=" col-md-6 row">
+                <div class="form-group col-md-3">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="nomeDoador" name="nomeDoador" placeholder="Digite o primeiro nome" required>
+                </div>
+                <div class="form-group col-md-3">
+                    <label for="nome">Sobrenome:</label>
+                    <input type="text" class="form-control" id="sobreNomeDoador" name="sobreNomeDoador" placeholder="Digite o Sobrenome" required>
+                </div>
+
+            </div>
+            <div class="form-group col-md-4">
+                <label for="phone">Telefone do contato:</label>
+                <input type="text" class="form-control" id="celular" name="celular" placeholder="Digite o telefone" required>
+            </div>
+
+
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+
+
+        </fieldset>
+
+        
 
     </form>
 </div>
